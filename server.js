1b783bcc406a0ca46249ae321bb14a2ad5f6e997
sangyeop yoo <vnvj0033@gmail.com>
2021-05-12 14:46:47 +0900

    express 사용

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/products", (req, res) => {
+  res.send("업로드 된 상품들입니다.");
+});
+
+app.post("/products", (req, res) => {
+  res.send("상품이 등록되었습니다.");
+});
+
+app.listen(port, () => {
+  console.log("그랩의 쇼핑물 서버가 돌아가고 있습니다");
+});
