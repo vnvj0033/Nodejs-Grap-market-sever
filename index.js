4054a07cda03e19641260890a93da5c22f676a0a
sangyeop yoo <vnvj0033@gmail.com>
2021-05-12 14:34:20 +0900

    add test get json

--- index.js
+++ index.js
@@ -3,7 +3,22 @@ const hostname = "127.0.0.1";
 const port = 8080;
 
 const server = http.createServer(function (req, res) {
-  console.log("REQUSET :", req);
+  const path = req.url;
+  const method = req.method;
+  if (path === "/products") {
+    if (method === "GET") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      const products = JSON.stringify([
+        {
+          name: "농구공",
+          price: 5000,
+        },
+      ]);
+      res.end(products);
+    } else if (method === "POST") {
+      res.end("생성되었습니다.");
+    }
+  }
   res.end("Hello Client!");
 });
 
